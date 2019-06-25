dcbe9eb4604170eba96212ae4b0a345bcddefe4e
toriumi <saori.toriumi.54@gmail.com>
2019-06-26 02:40:50 +0900

    wip

--- power.c
+++ power.c
@@ -2,13 +2,12 @@
 #include <stdio.h>
 #include "power.h"
 
-int power(int x, int i, int a)
-{
-    {
+int power(int x, int y, int a) {
     for(x = 0; x < 3; x++) {
-        a = x * i;
-        printf("%d の %d乗 = %d¥n",power(x, i, a));
-        return 0;
+        for(y = 0; y < 4; y++) {
+            a = x * y;
+            printf("%d の %d 乗 = %d\n", power(x, y, a));
+            return 0;
         }
     }
 }
