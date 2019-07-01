c8f948db4edf60bbd00f60b6827cd05b21590388
toriumi <saori.toriumi.54@gmail.com>
2019-07-01 16:43:32 +0900

    Fix indent

--- power.c
+++ power.c
@@ -4,19 +4,20 @@ int power(int x, int y);
 
 int main()
 {
-int i;
+    int i;
 
-for (i = 0; i< 5; ++i)
-    printf("%d乗 -1の冪乗: %d -2の冪乗: %d\n", i, power(-1,i), power(2,i));
-    return 0;
+    for (i = 0; i< 5; ++i)
+        printf("%d乗 -1の冪乗: %d -2の冪乗: %d\n", i, power(-1,i), power(2,i));
+        return 0;
 }
 
 int power(int base, int y)
 {
-int i, s;
+    int i, s;
 
-s = 1;
-for (i = 1; i <= y; ++i)
-    s = s * base;
-    return s;
+    s = 1;
+
+    for (i = 1; i <= y; ++i)
+        s = s * base;
+        return s;
 }
