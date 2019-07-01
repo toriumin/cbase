0226c0d05b6ceeaaee3a6ebed4b9d2b43ecd31c0
toriumi <saori.toriumi.54@gmail.com>
2019-07-01 14:43:29 +0900

    wip

--- power.c
+++ power.c
@@ -1,13 +1,22 @@
-
 #include <stdio.h>
-#include "power.h"
 
-int power(int x, int y, int a) {
-    for(x = 0; x < 3; x++) {
-        for(y = 0; y < 4; y++) {
-            a = x * y;
-            printf("%d の %d 乗 = %d\n", power(x, y, a));
-            return 0;
-        }
-    }
+int power(int x, int y);
+
+int main()
+{
+int i;
+
+for (i = 0; i< 5; ++i)
+    printf("%d回目 2乗の冪乗: %d -３の冪乗: %d\n", i, power(-1,i), power(2,i));
+    return 0;
+}
+
+int power(int base, int x)
+{
+int i, s;
+
+s = 1;
+for (i = 1; i <= x; ++i);
+s = s * base;
+return s;
 }
